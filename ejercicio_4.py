7b5c272d290c0373832b3bc9b72c1d4ad40ee7de
martin_frontroth <frontrothm@gmail.com>
2021-11-16 13:34:57 -0300

    ej_4

---
+++ ejercicio_4.py
@@ -0,0 +1,7 @@
+# Escribir una funcion que reciba un string 
+# con nombre y apellido y devuelva un string con el nombre y 
+# apellido pero con capitalizacion(primera letra mayuscula).
+
+def capitalizacion(cadena):
+    return cadena.title()
+
